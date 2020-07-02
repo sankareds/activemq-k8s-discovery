473b6dc2087235609e9f17895c18a653ffe3115a
Sankar Kanagarathinam <sankar.kanagarathinam@dowjones.com>
2020-07-02 13:07:26 -0400

    added queue browser code

--- src/test/java/org/apache/activemq/transport/discovery/k8s/ActiveMqTest.java
+++ src/test/java/org/apache/activemq/transport/discovery/k8s/ActiveMqTest.java
@@ -3,23 +3,18 @@ package org.apache.activemq.transport.discovery.k8s;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
-import javax.jms.Connection;
-import javax.jms.DeliveryMode;
-import javax.jms.Destination;
-import javax.jms.ExceptionListener;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
+import javax.jms.*;
 
 import org.apache.activemq.ActiveMQSession;
 import org.apache.activemq.command.BrokerInfo;
+import org.apache.activemq.command.ConnectionError;
 import org.apache.activemq.transport.TransportListener;
 import org.apache.activemq.transport.failover.FailoverTransport;
 import org.junit.Test;
-import javax.jms.ConnectionMetaData;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Enumeration;
 
@@ -36,23 +31,83 @@ import static org.junit.Assert.assertTrue;
 
 public class ActiveMqTest {
 
+    public static int groupSize = 1000;
+    public static String groupName = "groupz";
+
     @Test
     public void canCreateKubernetesDiscoveryAgent() throws IOException, InterruptedException {
-        //thread(new HelloWorldProducer(), false);
-//        thread(new HelloWorldConsumer(), false);
+
+//        StringBuilder writeBuilder = new StringBuilder();
+//        Random random = new Random();
+//        String alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnO oPpQqRrSsTtUuVvWwXxYyZz";
+//        int alphabetLength = alphabet.length();
+//        int charTotal = 999999;
 //
-//        Thread.sleep(1000000);
-
-
-////        while (true) {
-            HelloWorldProducer p = new HelloWorldProducer();
-            p.run();
-           // Thread.sleep(10000);
-//        HelloWorldConsumer c = new HelloWorldConsumer() ;
-//        c.run();
-
+//        for (int j = 0; j < charTotal; j++) {
+//            writeBuilder.append(alphabet.charAt(random.nextInt(alphabetLength)));
 //        }
+//
+//        try {
+//
+//        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(tcp://localhost:61616)");
+//
+//        // Create a Connection
+//        //connectionFactory.setAlwaysSyncSend(true);
+//        Connection connection = null;
+//        try {
+//            connection = connectionFactory.createConnection();
+//        } catch (JMSException e) {
+//            e.printStackTrace();
+//        }
+//
+//        connection.start();
+//
+//        // Create a Session
+//        BrokerInfo brokerInfo = ((ActiveMQConnection) connection).getBrokerInfo();
+//        System.out.println(brokerInfo.getBrokerId());
+//
+//
+////        while(!brokerInfo.getBrokerId().toString().equals("activemq-2")){
+////            connection.close();
+////            connection = connectionFactory.createConnection();
+////            connection.start();
+////            brokerInfo = ((ActiveMQConnection) connection).getBrokerInfo();
+////
+////        }
+//
+//        System.out.println(brokerInfo.getBrokerId());
+//
+//        for (int i=0; i<20; i++){
+//            thread(new HelloWorldProducer(writeBuilder, connection), false);
+//        }
+//
+//
+//        while(true){
+//            Thread.sleep(1000);
+//        }
+////        thread(new HelloWorldConsumer(), false);
+////
+////        Thread.sleep(1000000);
+//
+//
+//////        while (true) {
+////            HelloWorldProducer p = new HelloWorldProducer();
+////            p.run();
+//           // Thread.sleep(10000);
+////        HelloWorldConsumer c = new HelloWorldConsumer() ;
+////        c.run();
+//
+//        HelloWorldBrowser b = new HelloWorldBrowser();
+//        b.run();
+//
+////        }
+//
+//    } catch (JMSException e) {
+//        e.printStackTrace();
+//    }
 
+                HelloWorldBrowser b = new HelloWorldBrowser();
+                b.run();
     }
 
 
@@ -78,13 +133,19 @@ public class ActiveMqTest {
 
     public static class HelloWorldProducer implements Runnable, ExceptionListener, TransportListener {
 
+        private StringBuilder writeBuilder;
+        HelloWorldProducer(StringBuilder writeBuilder, Connection connection){
+            this.writeBuilder = writeBuilder;
+            this.connection = connection;
+        }
+
         Connection connection = null;
         public void run() {
 
             Random random = new Random();
             String alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnO oPpQqRrSsTtUuVvWwXxYyZz";
             int alphabetLength = alphabet.length();
-            int charTotal = 9999;
+            int charTotal = 9999999;
 
             //ActiveMQConnectionFactory connctionFactory = connctionFactory.setProducerWindowSize(1024000);
 
@@ -93,23 +154,23 @@ public class ActiveMqTest {
                 try {
                     // Create a ConnectionFactory
                     //ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("fanout:(static:(ssl://activemq-0.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-1.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-2.contentmgmt.stag.pib.dowjones.io:61616))?jms.alwaysSyncSend=true&randomize=true&fanOutQueues=true&minAckCount=3");
-                    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(ssl://activemq.contentmgmt.prod.pib.dowjones.io:61616)?jms.alwaysSyncSend=true");
+                    //ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(ssl://activemq-2.contentmgmt.int.pib.dowjones.io:61616)?jms.useAsyncSend=false");
 
                     // Create a Connection
-                    connection = connectionFactory.createConnection();
-                    connection.start();
+//                    connectionFactory.setAlwaysSyncSend(true);
+//                    connection = connectionFactory.createConnection();
 
-                    ((ActiveMQConnection) connection).addTransportListener(this);
+//                    connection.start();
+
+                    //((ActiveMQConnection) connection).onException(this);
+                    //((ActiveMQConnection) connection).addTransportListener(this);
                     System.out.println(((ActiveMQConnection) connection).getTransport().getRemoteAddress());
 
-                    // Create a Session
-                    BrokerInfo brokerInfo = ((ActiveMQConnection) connection).getBrokerInfo();
-                    System.out.println(brokerInfo.getBrokerId());
                     ActiveMQSession session = (ActiveMQSession) connection.createSession(false, ActiveMQSession.AUTO_ACKNOWLEDGE);
                     // Create the destination (Topic or Queue)
-                    Destination destination = session.createQueue("mytest2");
+                    Destination destination = session.createQueue("myqueue");
 
-
+                    //Destination destination = session.createTopic("VirtualTopic.MetaData");
                     // Create a MessageProducer from the Session to the Topic or Queue
 
                     MessageProducer producer = session.createProducer(destination);
@@ -117,14 +178,10 @@ public class ActiveMqTest {
 
                     // Create a messages
 
-                    StringBuilder writeBuilder = new StringBuilder();
-                    for (int j = 0; j < charTotal; j++) {
-                        writeBuilder.append(alphabet.charAt(random.nextInt(alphabetLength)));
-                    }
 
                     for (int g = 0; g < 10; g++) {
-                        String group = "groupy" + g;
-                        for (int i = 1; i <= 1000; i++) {
+                        String group = groupName + g;
+                        for (int i = 1; i <= groupSize; i++) {
 
 
                             String text = writeBuilder + Thread.currentThread().getName() + " : " + this.hashCode();
@@ -133,17 +190,17 @@ public class ActiveMqTest {
                             message.setStringProperty("JMSXGroupID", group);
                             message.setIntProperty("JMSXGroupSeq", i);
 
-                            if (i == 1000) {
+                            if (i == groupSize) {
                                 message.setIntProperty("JMSXGroupSeq", -1);
                                 producer.send(message);
-                                Thread.sleep(10000);
+                                Thread.sleep(1000);
                                 //System.exit(0);
                             }else {
                                 // Tell the producer to send the message
 
                                 producer.send(message);
                                 System.out.println("Sent message: " + message.hashCode() + " : " + Thread.currentThread().getName());
-                                Thread.sleep(1);
+                                //Thread.sleep(1);
                             }
                         }
 
@@ -179,12 +236,12 @@ public class ActiveMqTest {
 
         @Override
         public void onCommand(Object o) {
-            System.out.println("on command");
+            System.out.println(((ConnectionError)o).getException());
         }
 
         @Override
         public void onException(IOException e) {
-            System.out.println("failover error");
+            System.out.println("Caught: " + e);
         }
 
         @Override
@@ -195,6 +252,105 @@ public class ActiveMqTest {
         @Override
         public void transportResumed() {
             System.out.println("on resume");
+        }
+    }
+
+
+    public static class HelloWorldBrowser implements Runnable, ExceptionListener {
+        HashMap<String, ArrayList<Integer>> groupCounts = new HashMap<String,  ArrayList<Integer>>();
+        public void run() {
+            Connection connection = null;
+
+
+                try {
+
+                    // Create a ConnectionFactory
+
+                    //ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(ssl://activemq-0.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-1.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-2.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-3.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-4.contentmgmt.stag.pib.dowjones.io:61616)?randomize=false");
+
+                    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(ssl://activemq.contentmgmt.int.pib.dowjones.io:61616?keepAlive=true&useKeepAlive=true)?initialReconnectDelay=5000");
+
+                    // Create a Connection
+                    connection = connectionFactory.createConnection();
+                    connection.start();
+
+
+
+
+                    // Create a Session
+                    BrokerInfo brokerInfo = ((ActiveMQConnection) connection).getBrokerInfo();
+                    System.out.println(brokerInfo.getBrokerId());
+
+                    connection.setExceptionListener(this);
+
+
+                    // Create a Session
+                    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                    Queue queue = session.createQueue("Consumer.dac.VirtualTopic.daq2dac");
+                    
+                    QueueBrowser browser = session.createBrowser(queue);
+
+                    Enumeration msgs = browser.getEnumeration();
+
+                    if ( !msgs.hasMoreElements() ) {
+                        System.out.println("No messages in queue");
+                    } else {
+                        while (msgs.hasMoreElements()) {
+                            Message message = (Message)msgs.nextElement();
+
+                            String groupId = message.getStringProperty("JMSXGroupID");
+                            int groupSeq = message.getIntProperty("JMSXGroupSeq");
+                            if(message.getJMSRedelivered()){
+                                System.out.println("redelivered");
+                            }
+
+                            if (groupCounts.get(groupId) == null) {
+                                groupCounts.put(groupId, new ArrayList<Integer>());
+                            }
+
+                            ArrayList<Integer> groupSeqList = groupCounts.get(groupId);
+                            groupSeqList.add(groupSeq);
+
+
+                            System.out.println("Message: " + message);
+                        }
+
+
+                    }
+
+                    System.out.print(groupCounts.keySet());
+                    for (int i = 0; i < groupCounts.keySet().size() ; i++) {
+                        String key = (String)groupCounts.keySet().toArray()[i];
+                        System.out.print(key + ":");
+                        System.out.print(((ArrayList<Integer>)groupCounts.get(key)).size());
+                        System.out.println();
+                    }
+//                    System.out.print(((ArrayList<Integer>)groupCounts.get("BOARDR")).size());
+//                    System.out.print(((ArrayList<Integer>)groupCounts.get("BOARDO")).size());
+
+                    browser.close();
+                    session.close();
+                    connection.close();
+                } catch (Exception e) {
+                    try {
+                        connection.close();
+                        Thread.sleep(1000);
+                    } catch (JMSException e1) {
+                        e1.printStackTrace();
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    System.out.println("Caught: " + e);
+                    e.printStackTrace();
+
+                }
+
+
+        }
+
+        public synchronized void onException(JMSException ex) {
+            System.out.println("JMS Exception occured.  Shutting down client.");
         }
     }
 
@@ -211,11 +367,18 @@ public class ActiveMqTest {
 
                     //ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(ssl://activemq-0.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-1.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-2.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-3.contentmgmt.stag.pib.dowjones.io:61616,ssl://activemq-4.contentmgmt.stag.pib.dowjones.io:61616)?randomize=false");
 
-                    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(ssl://activemq.contentmgmt.prod.pib.dowjones.io:61616?keepAlive=true&useKeepAlive=true)?initialReconnectDelay=5000");
+                    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("failover:(ssl://activemq.contentmgmt.int.pib.dowjones.io:61616?keepAlive=true&useKeepAlive=true)?initialReconnectDelay=5000");
 
-                    // Create a Connection
+                        // Create a Connection
                     connection = connectionFactory.createConnection();
                     connection.start();
+
+
+
+
+                    // Create a Session
+                    BrokerInfo brokerInfo = ((ActiveMQConnection) connection).getBrokerInfo();
+                    System.out.println(brokerInfo.getBrokerId());
 
                     connection.setExceptionListener(this);
 
@@ -223,9 +386,11 @@ public class ActiveMqTest {
                     // Create a Session
                     Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
+
+
                     // Create the destination (Topic or Queue)
                     //seq_test5?consumer.prefetchSize=1
-                    Destination destination = session.createQueue("mytest2?consumer.prefetchSize=1&keepAlive=true");
+                    Destination destination = session.createQueue("test?consumer.prefetchSize=1&keepAlive=true");
 
                     // Create a MessageConsumer from the Session to the Topic or Queue
                     MessageConsumer consumer = session.createConsumer(destination);
@@ -235,7 +400,7 @@ public class ActiveMqTest {
 
                     for (int i = 1; i < 100000; i++) {
 
-                        Message message = consumer.receive(10000);
+                        Message message = consumer.receive(1000);
 
 
                         if (message instanceof TextMessage) {
@@ -249,7 +414,12 @@ public class ActiveMqTest {
                             TextMessage textMessage = (TextMessage) message;
                             String text = textMessage.getText();
 
-                            if (groupSeqList.size() == 1000) {
+                            if (groupSeqList.size() == groupSize) {
+                                try {
+                                    Files.write(Paths.get(groupId.substring(0, groupId.length()-1)+".txt"), (groupId+"\n").getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+                                }catch (IOException e) {
+                                    //exception handling left as an exercise for the reader
+                                }
                                 System.out.println("group " + groupId + "finished succesfully");
                                 System.out.println(groupSeqList.toString());
                                 groupCounts.put(groupId, null);
@@ -277,7 +447,7 @@ public class ActiveMqTest {
                     } catch (Exception e) {
                         try {
                             connection.close();
-                            Thread.sleep(40000);
+                            Thread.sleep(1000);
                         } catch (JMSException e1) {
                             e1.printStackTrace();
                         } catch (InterruptedException e1) {
